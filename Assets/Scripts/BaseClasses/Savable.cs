6695dadfbfd484aec30ecea1aa0c2eed4634e20c
Antonio Hollerman <toniohollerprof@gmail.com>
2024-10-04 01:20:25 -0400

    Stable Version and Initialize Base Classes

---
+++ Assets/Scripts/BaseClasses/Savable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BaseClasses
+{
+    public class Savable : MonoBehaviour
+    {
+        protected virtual void StartWrapper()
+        {
+            
+        }
+
+        protected virtual void UpdateWrapper()
+        {
+            
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartWrapper();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateWrapper();
+        }
+    }
+}
