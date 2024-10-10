cca7331df91940ea4af581a4e9cc225b426946c7
AntonioHollerman <toniohollerProf@gmail.com>
2024-10-10 13:17:25 -0400

    populate Technique.cs with code

--- Assets/Scripts/BaseClasses/Technique.cs
+++ Assets/Scripts/BaseClasses/Technique.cs
@@ -1,7 +1,10 @@
 ﻿namespace BaseClasses
 {
-    public class Technique
+    public abstract class Technique
     {
-        
+        public int ManaCost { get; protected set; }
+        public abstract void Cast(CharacterSheet caster);
+        public abstract void Effect(CharacterSheet cs);
+        public abstract string GetIconPath();
     }
 }
